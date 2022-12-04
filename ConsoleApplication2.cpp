43c5526ef46364e5d5ae23a1ae9cd4d82b22e560
lady-gagaga <pasha.zemliansky@yandex.ru>
2022-12-04 18:18:10 +0300

    cpp1

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,6 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int n = 245;
+	cout << n / 100 << endl;
+}
---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+// Function to reverse a string
+string reverse(string const& s)
+{
+
+    string rev;
+    for (int i = s.size() - 1; i >= 0; i--) {
+        rev = rev.append(1, s[i]);
+    };
+
+    return rev;
+}
+
+int main()
+
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string s("i don't know");
+
+    s = reverse(s);
+    cout << s;
+
+    return 0;
+}
