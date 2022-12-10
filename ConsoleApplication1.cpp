b6ec3c13c5bb2a34bb236e4f664e09a068eb0c6c
lady-gagaga <pasha.zemliansky@yandex.ru>
2022-12-10 17:43:42 +0300

    Revert "Revert "cpp1""
    This reverts commit 27c2b22aa9813f88d2456c6d44b3f34abef85a73.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << (a * a) / (b * b) << endl;
+}
